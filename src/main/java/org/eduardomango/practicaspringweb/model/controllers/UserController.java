7124ff4a559795dacf96c56db96d3d8871afb997
ivan <ivandiclementejesus@gmail.com>
2026-04-14 12:15:13 -0300

    Unas cosas mas del response entity

--- src/main/java/org/eduardomango/practicaspringweb/model/controllers/UserController.java
+++ src/main/java/org/eduardomango/practicaspringweb/model/controllers/UserController.java
@@ -3,6 +3,8 @@ package org.eduardomango.practicaspringweb.model.controllers;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.eduardomango.practicaspringweb.model.entities.UserEntity;
 import org.eduardomango.practicaspringweb.model.services.UserService;
@@ -21,17 +23,30 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<UserEntity>> retornarTodosLosUsuarios(){
-        return ResponseEntity.ok(userService.findAll());
+
+        if(userService.findAll()!=null){
+            return ResponseEntity.ok(userService.findAll());
+        }else{
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
     }
 
     @GetMapping("/{id}")
-    public UserEntity retornarUsuarios(@PathVariable long id){
-        return userService.findById(id);
+    public ResponseEntity<UserEntity> retornarUsuarios(@PathVariable long id){
+
+        if(userService.findById(id)!=null){
+            return ResponseEntity.ok(userService.findById(id));
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
     }
 
     @PostMapping
-    public void crearUsuario(@RequestBody UserEntity user){
+    public ResponseEntity<UserEntity> crearUsuario(@RequestBody UserEntity user){
         userService.save(user);
+        return ResponseEntity.ok().body(user);
     }
 
     @PutMapping("/{id}")
